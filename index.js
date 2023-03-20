fbd58da8d1faca10de4f08a5d03933c0b6e3e44b
NaneKarapetyan304 <nanekarapetyann304@gmail.com>
2023-03-20 15:19:32 +0400

    some changes

---
+++ index.js
@@ -0,0 +1,6 @@
+responsive = document.querySelector(".responsive");
+        responsive.onclick = function(){
+            navBar = document.querySelector(".nav");
+            navBar.classList.toggle("active")
+        }
+let email = document.querySelector(".email_input");
